c5f1e89b9c7b86f5ea95a058fe031f34ab815579
Katie Hoffman <ktahoffman@gmail.com>
2014-12-11 12:24:17 -0500

    refac spec

--- lib/stock_market.rb
+++ lib/stock_market.rb
@@ -3,6 +3,7 @@ require 'json'
 
 def get_buy_and_sell_times(json_file_path)
   results = {:buy => nil, :sell => nil}
+  differences = []
   data = JSON.parse(File.read(json_file_path))
   data.each do |min, price|
     
--- spec/stock_market_spec.rb
+++ spec/stock_market_spec.rb
@@ -1,35 +1,34 @@
 describe '#get_buy_and_sell_times' do
+  
+  let(:jamba_juice) { get_buy_and_sell_times("spec/fixtures/jamba-juice-stock.json") }
+  let(:apple)       { get_buy_and_sell_times("spec/fixtures/apple-stock.json")       }
+  let(:tesla)       { get_buy_and_sell_times("spec/fixtures/tesla-stock.json")       }
 
   it 'returns a hash' do
-    expect(get_buy_and_sell_times("spec/fixtures/jamba-juice-stock.json").class).to eq(Hash)
+    expect(jamba_juice.class).to eq(Hash)
   end
 
   it 'contains two keys, :buy and :sell' do
-    result = get_buy_and_sell_times("spec/fixtures/jamba-juice-stock.json")
-    [:buy, :sell].each { |k| expect(result[k]).to_not eq(nil) }
+    [:buy, :sell].each { |k| expect(jamba_juice[k]).to_not eq(nil) }
   end
 
   it 'contains two string values for :buy and :sell' do
-    result = get_buy_and_sell_times("spec/fixtures/jamba-juice-stock.json")
-    [:buy, :sell].each { |k| expect(result[k].class).to eq(String) }
+    [:buy, :sell].each { |k| expect(jamba_juice[k].class).to eq(String) }
   end
 
   it 'correctly predicts when to buy and sell Jamba Juice stock' do
-    result = get_buy_and_sell_times("spec/fixtures/jamba-juice-stock.json")
-    expect(result[:buy]).to eq()
-    expect(result[:sell]).to eq()
+    expect(jamba_juice[:buy]).to eq()
+    expect(jamba_juice[:sell]).to eq()
   end
 
   it 'correctly predicts when to buy and sell Apple stock' do
-    result = get_buy_and_sell_times("spec/fixtures/apple-stock.json")
-    expect(result[:buy]).to eq()
-    expect(result[:sell]).to eq()
+    expect(apple[:buy]).to eq()
+    expect(apple[:sell]).to eq()
   end
 
   it 'correctly predicts when to buy and sell Tesla stock' do
-    result = get_buy_and_sell_times("spec/fixtures/tesla-stock.json")
-    expect(result[:buy]).to eq()
-    expect(result[:sell]).to eq()
+    expect(tesla[:buy]).to eq()
+    expect(tesla[:sell]).to eq()
   end
 
 end
